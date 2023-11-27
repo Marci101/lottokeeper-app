b4eb104bfba37f62f87ac99ad2d9076b7afcc534
Marton Szitas <szitas.marton.tamas@gmail.com>
2023-11-27 14:42:26 +0100

    Style refined

--- src/ui/pages/account/UserAccountPage.jsx
+++ src/ui/pages/account/UserAccountPage.jsx
@@ -1,11 +1,16 @@
+import { useSelector } from 'react-redux';
 import ButtonRounded from '../../components/button/ButtonRounded';
 import FormYourName from '../../components/form/FormYourName';
 import './UserAccountPage.css';
 
 export default function UserAccountPage() {
+  const userName = useSelector((state) => state.yourName.userName);
+
   return (
     <div id="user-account">
-      <p>Your nickname:</p>
+      <div id="your-name">
+        <p>Your nickname:&nbsp;</p><p id="your-nickname">{userName}</p>
+      </div>
       <FormYourName buttonText='Edit' />
       <p>Your balance: 10.000 Akce</p>
       <ul>
