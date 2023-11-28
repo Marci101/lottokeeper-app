0422fa720ce4c67ad79a64202f4f2cf40fa91767
Marton Szitas <szitas.marton.tamas@gmail.com>
2023-11-28 06:44:14 +0100

    Homepage pimped a bit

--- src/ui/pages/home/HomePage.jsx
+++ src/ui/pages/home/HomePage.jsx
@@ -3,8 +3,9 @@ import "./homePage.css";
 export default function HomePage() {
   return (
     <div id="home-page">
-      <p>Under construction!</p>
-      <p>Homepage Block!</p>
+      <p>Welcome to the Game!</p>
+      <p id="main-prize">Grab the chance and aim the main prize!</p>
+      <p>Go further!</p>
     </div>
   );
 }
